32d21faf3c85b0f6a8bb4d9e6e00a1636798a4d4
Leonardo Valderrama <leonardov7@gmail.com>
2022-10-02 00:28:32 -0500

    Creación de botones en user y admin

--- lib/Roles/MenuAdmin.dart
+++ lib/Roles/MenuAdmin.dart
@@ -2,20 +2,50 @@ import 'package:flutter/material.dart';
 
 import '../DTO/UserObject.dart';
 
-class MenuAdmin extends StatefulWidget{
+class MenuAdmin extends StatefulWidget {
   final UserObject userOb1;
   MenuAdmin(this.userOb1);
   MenuAdminApp createState() => MenuAdminApp();
 }
-class MenuAdminApp extends State<MenuAdmin>{
+
+class MenuAdminApp extends State<MenuAdmin> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hola '+widget.userOb1.nombre+ ' administrador'),
+        title: Text('Hola ' + widget.userOb1.nombre + ' administrador'),
         backgroundColor: Colors.indigo,
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 10, top: 40, right: 10, bottom: 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.indigo, minimumSize: Size(400, 50)),
+                onPressed: () {},
+                child: Text(
+                  'Gestión de usuario',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, top: 20, right: 10, bottom: 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.indigo, minimumSize: Size(400, 50)),
+                onPressed: () {},
+                child: Text(
+                  'Gestión de usuario',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/Roles/MenuUser.dart
+++ lib/Roles/MenuUser.dart
@@ -2,20 +2,74 @@ import 'package:flutter/material.dart';
 
 import '../DTO/UserObject.dart';
 
-class MenuUser extends StatefulWidget{
+class MenuUser extends StatefulWidget {
   final UserObject userOb1;
   MenuUser(this.userOb1);
   MenuUserApp createState() => MenuUserApp();
 }
-class MenuUserApp extends State<MenuUser>{
+
+class MenuUserApp extends State<MenuUser> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hola usuario '+widget.userOb1.nombre),
-        backgroundColor: Colors.orange,
+        title: Text('Hola usuario ' + widget.userOb1.nombre),
+        backgroundColor: Colors.deepPurple,
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 10, top: 40, right: 10, bottom: 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple, minimumSize: Size(400, 50)),
+                onPressed: () {},
+                child: Text(
+                  'Localización',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, top: 20, right: 10, bottom: 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple, minimumSize: Size(400, 50)),
+                onPressed: () {},
+                child: Text(
+                  'Predicción del clima',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, top: 20, right: 10, bottom: 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple, minimumSize: Size(400, 50)),
+                onPressed: () {},
+                child: Text(
+                  'Posición Geográfica',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10, top: 20, right: 10, bottom: 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple, minimumSize: Size(400, 50)),
+                onPressed: () {},
+                child: Text(
+                  'Gestíon de usuario',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
